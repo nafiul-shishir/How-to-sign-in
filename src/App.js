ef9e64b06c32a61a7bb2dd6fcbc85154439027d8
nafiul-shishir <nafiul.shishir@gmail.com>
2021-05-11 16:10:30 +0600

    sign in options

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user,setUser] = useState({
+    isSignedIn: false,
+    name:'',
+    email: '',
+    photo: ''
+  })
+
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, photoURL, email} = res.user
+
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+
+      setUser(signedInUser);
+      
+      console.log(displayName, photoURL, email);
+    })
+
+    .catch(error => {
+      console.log(error);
+      console.log(error.message);
+    })
+  }
+
+  const handelSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const signedOutuser = {isSignedIn: false,
+      name: '',
+      photo:'',
+      email:'',
+    }
+
+    setUser(signedOutuser)
+
+    })
+
+    .catch(error => {
+
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+        user.isSignedIn ? <button onClick={handelSignOut}> Sign Out</button>: 
+        <button onClick={handleSignIn}> Sign In</button>
+      }
+      {
+        user.isSignedIn &&
+         <div>
+           <p> Welcome, {user.name}</p>
+           <p> Email: {user.email}</p>
+           <img src={user.photo} alt="" />
+         </div>
+      }
+      
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBuuRHN-GVRgOuTDrEV5QyWiBtpd-tTlw",
+    authDomain: "ema-jhon-pro.firebaseapp.com",
+    projectId: "ema-jhon-pro",
+    storageBucket: "ema-jhon-pro.appspot.com",
+    messagingSenderId: "179533133812",
+    appId: "1:179533133812:web:eec711d65da7fa470f6ba1"
+  };
+
+  export default firebaseConfig;
